2c1862e344d4d21e1662f6ee8c4de8e9b0a0cc3b
GitLuoSiyu <1514813390@qq.com>
2019-03-25 10:13:13 +0800

    add vue router

--- ntv-vue/src/App.vue
+++ ntv-vue/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/gameOne">gameOne</router-link>
+      <router-link to="/gameTwo">gameTwo</router-link>
+      <router-link to="/shop">shop</router-link>
+      <router-link to="/purchase">purchase</router-link>
     </div>
     <router-view />
   </div>
--- ntv-vue/src/router.ts
+++ ntv-vue/src/router.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
+
+// page components
 import Home from "./views/Home.vue";
+import Game from "./views/Game.vue";
+import Levels from "./views/Levels.vue";
+import Shop from "./views/Shop.vue"
 
 Vue.use(Router);
 
@@ -12,13 +17,25 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/menu",
+      name: "menu",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/Menu.vue")
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: Game
+    },
+    {
+      path: "/levels",
+      name: "Levels",
+      component: Levels
+    },
+    {
+      path: "/shop",
+      name: "shop",
+      component: Shop
     }
   ]
 });
