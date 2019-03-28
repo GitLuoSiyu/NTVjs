9e6fec8df2aea42d4fbb96e3b9c50fdf8b88d75f
GitLuoSiyu <1514813390@qq.com>
2019-03-28 14:13:07 +0800

    add weex

---
+++ ntv-weex/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
